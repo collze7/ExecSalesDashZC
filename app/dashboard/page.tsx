93f13ac4e6864a5c017ab04dcae1019030504b7b
collze7 <121809317+collze7@users.noreply.github.com>
2026-04-17 13:25:18 -0400

    Add files via upload

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { useState } from 'react'
-import Image from 'next/image'
-import DashboardContent from '@/components/DashboardContent'
+import DashboardContent from '../../components/DashboardContent'
 
 export default function DashboardPage() {
   return <DashboardContent />
